bc0019162a7b92a9f40d317195850fd32f0f947f
Mohamed Rostom <mohamedrostom62@gmail.com>
2022-05-16 22:56:17 +0200

    Rusty bot v1

---
+++ test.py
@@ -0,0 +1,106 @@
+from user_simulator import UserSimulator
+from dqn_agent import DQNAgent
+from state_tracker import StateTracker
+import pickle, argparse, json, math
+from utils import remove_empty_slots
+from user import User
+from dialogue_config import all_responses
+import os
+import random
+
+os.system('color')
+
+
+if __name__ == '__main__':
+    # Can provide constants file path in args OR run it as is and change 'CONSTANTS_FILE_PATH' below
+    # 1) In terminal: python train.py --constants_path "constants.json"
+    # 2) Run this file as is
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--constants_path', dest='constants_path', type=str, default='')
+    args = parser.parse_args()
+    params = vars(args)
+
+    # Load constants json into dict
+    CONSTANTS_FILE_PATH = 'constants.json'
+    if len(params['constants_path']) > 0:
+        constants_file = params['constants_path']
+    else:
+        constants_file = CONSTANTS_FILE_PATH
+
+    with open(constants_file) as f:
+        constants = json.load(f)
+
+    # Load run constants
+    run_dict = constants['run']
+    USE_USERSIM = run_dict['usersim']
+    NUM_EP_TEST= run_dict['num_ep_run']
+    MAX_ROUND_NUM = run_dict['max_round_num']
+    EMC_ENABLED = run_dict['emc_enabled']
+
+
+    # Init Objects
+    if USE_USERSIM:
+        user = UserSimulator(constants)
+    else:
+        user = User(constants)
+
+    state_tracker = StateTracker(constants)
+    dqn_agent = DQNAgent(state_tracker.get_state_size(), constants['agent'])
+
+
+def test_run():
+    print('Testing Started...')
+    episode = 0
+    fail_counter = 0
+    while episode < NUM_EP_TEST:
+        episode_reset()
+        episode += 1
+        ep_reward = 0
+        done = False
+
+        state = state_tracker.get_state()
+        while not done:
+
+            if EMC_ENABLED:
+                # Add error to state for more robustness
+                randIdx = random.randint(0, len(state)-1)
+                randtemp = random.random()
+                if randtemp <= 0.15:
+                    state[randIdx] = 1 if state[randIdx] == 0 else 0
+                    print('\033[91m' + f"state after error = {state}" + '\033[0m')
+
+            agent_action_index, agent_action = dqn_agent.get_action(state)
+            state_tracker.update_state_agent(agent_action)
+            user_action, reward, done, success = user.step(agent_action)
+
+            if reward == -1:
+                fail_counter += 1
+            
+            ep_reward += reward
+
+            state_tracker.update_state_user(user_action)
+
+            state = state_tracker.get_state(done)
+
+            print(f"Agent action: {agent_action}")
+            print('\x1b[6;30;42m' + f"Agent Response ==> {all_responses[agent_action_index]}" + '\x1b[0m')
+
+        print('Episode: {} Success: {} Reward: {}'.format(episode, success, ep_reward))
+    
+    print("=====================")
+    avg_reward = ((NUM_EP_TEST - fail_counter) / NUM_EP_TEST) * 100
+    print(f"Average reward for {NUM_EP_TEST} testing episodes = {avg_reward}%")
+    print(f"Failed episodes count = {fail_counter}")
+    print('...Testing Ended')
+
+
+def episode_reset():
+    state_tracker.reset()
+    user_action = user.reset()
+
+    state_tracker.update_state_user(user_action)
+
+    dqn_agent.reset()
+
+
+test_run()
---
+++ user.py
@@ -0,0 +1,97 @@
+from dialogue_config import FAIL, SUCCESS, usersim_intents, all_slots
+from utils import reward_function
+from nlu import NLU
+
+class User:
+    def __init__(self, constants):
+        self.max_round = constants['run']['max_round_num']
+        self.nlu = NLU()
+
+    
+    def reset(self):
+        self.user_last_response = None
+        return self._use_nlu()
+        # return self._return_response()
+
+    
+    def _use_nlu(self):
+        while True:
+            input_string = input("\x1b[93m" + "User input ==> " + "\x1b[0m")
+            semanticFrame =  self.nlu.getSemanticFrame(input_string)
+            print("\x1b[93m" + f"User semantic frame: {semanticFrame}" + "\x1b[0m")
+            self.user_last_response = semanticFrame
+            return semanticFrame
+
+
+    def _return_response(self):
+        response = {'intent': '', 'inform_slots': {}, 'request_slots': {}}
+        while True:
+            input_string = input("Response: ")
+            chunks = input_string.split('/')
+
+            # print(chunks)
+
+            intent_correct = True
+            if chunks[0] not in usersim_intents:
+                intent_correct = False
+            response['intent'] = chunks[0]
+
+            informs_correct = True
+            if len(chunks[1]) > 0:
+                inform_items_list = chunks[1].split(', ')
+                for inf in inform_items_list:
+                    inf = inf.split(': ')
+                    if inf[0] not in all_slots:
+                        informs_correct = False
+                        break
+                    response['inform_slots'][inf[0]] = "PLACEHOLDER"
+
+            requests_correct = True
+            if len(chunks[2]) > 0:
+                requests_key_list = chunks[2].split(', ')
+                for req in requests_key_list:
+                    if req not in all_slots:
+                        requests_correct = False
+                        break
+                    response['request_slots'][req] = 'UNK'
+                    
+
+            if intent_correct and informs_correct and requests_correct:
+                break
+
+        print("User input = " ,response)
+        self.user_last_response = response
+        return response
+
+
+    def _return_success(self):
+        """
+        Ask the user in console to input success (-1, 0 or 1) for (loss, neither loss nor win, win).
+
+        Returns:
+            int: Success: -1, 0 or 1
+        """
+
+        success = -2
+        while success not in (-1, 0, 1):
+            success = int(input('Success?: '))
+        return success
+
+
+    def step(self, agent_action):
+        user_response = {'intent': '', 'request_slots': {}, 'inform_slots': {}}
+        reward = 0
+
+        agent_informs = agent_action['inform_slots']
+        informed_key = list(agent_informs.keys())[0]
+
+        if self.user_last_response:
+            if self.user_last_response['intent'] == 'request':
+                key = list(self.user_last_response['request_slots'].keys())[0]
+                if informed_key == key:
+                    reward = SUCCESS
+                else:
+                    reward = FAIL
+        # reward = self._return_success()
+
+        return user_response, reward, True, True if reward == SUCCESS else False
